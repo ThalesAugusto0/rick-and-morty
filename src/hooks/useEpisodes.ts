5416f9b42999adc21f67de0bda22dbeb7f308747
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-03-28 23:40:36 -0300

    Refatorada a função 'getAllEpisodes', foi feito um loop para melhorar a leitura do código

--- src/hooks/useEpisodes.ts
+++ src/hooks/useEpisodes.ts
@@ -26,9 +26,13 @@ export default function useEpisodes() {
   }
 
   async function getAllEpisodes() {
-    const result = await api.get(
-      `/episode/[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]`
-    );
+    let ids = "";
+    for (let i = 1; i <= 51; i++) {
+      ids += i + ",";
+    }
+    ids = ids.slice(0, -1); 
+
+    const result = await api.get(`/episode/[${ids}]`);
 
     setAllEpisodes(result.data);
   }
